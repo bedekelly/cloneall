c396dda487470c07afa710f495b390aeb5c21ab0
bedekelly <bedekelly97@gmail.com>
2014-06-14 00:04:46 +0100

    Added 'update all' feature

--- cloneall.py
+++ cloneall.py
@@ -34,27 +34,34 @@ def get_json(url):
 def api_url(username):
     """Returns github api url for a given username."""
     return "https://api.github.com/users/{}/repos?per_page=100".format(username)
-
+    # Pass PHP argument per_page with value 100; this is max value.
 
 def parse_args():
     """
     Returns a dictionary in the format
     {username:<string>,
-     all:<boolean>}
-    The key 'all' refers to whether all repos should automatically be cloned.
+     dl_all:<boolean>,  # should download all repos
+     ud_all:<boolean>}  # should update all existing repos
     """
-
     args = sys.argv[1:]  # Don't include filename.
-    return_dict = {'username':"", 'all':False}  # Default values.
+
+    # By default the user has to pick whether to download/update all.
+    return_dict = {'username': "", 'dl_all': False, 'ud_all': False}
     if not args:
         return return_dict  # No input, get it from program.
 
     else:
-        if "--all" in args or "-a" in args:
-            # Set boolean, then remove all instances of "--all" or "-a" from list.
-            return_dict['all'] = True
-            args = [arg for element in args if element not in ["-a", "--all"]]
-        
+        if "--download-all" in args or "-a" in args:
+            # Set boolean, then remove all instances of "--all" or "-a"
+            return_dict['dl_all'] = True
+            args = [arg for arg in args 
+                if arg not in ["-a", "--download-all"]]
+        if "--update-all" in args or "-p" in args:
+            # Same as above, for update rather than download.
+            return_dict['ud_all'] = True
+            args = [arg for arg in args
+                if arg not in ["-p", "--update-all"]]
+
         # The element after "-u" or "--username" should be the username.
         if "--username" in args:
             return_dict['username'] = args[args.index("--username") + 1]
@@ -69,7 +76,7 @@ def print_repo_info(repo):
     print("Description:\n{}\n".format(repo['description']))
 
 
-def get_all():
+def should_download_all():
     """Provides a prompt to query whether all items should be downloaded."""
     while True:
         get_all = input("Download all? [Y/N] ")
@@ -77,6 +84,17 @@ def get_all():
             return True
         elif get_all in ['N', 'n']:
             return False
+
+
+def should_update_all():
+    """Provides a prompt to query whether all items should be updated."""
+    while True:
+        get_all = input("Update all? [Y/N] ")
+        if get_all in ['Y', 'y']:
+            return True
+        elif get_all in ['N', 'n']:
+            return False
+
 
 def get_username():
     """Provides a prompt to query the username to download from."""
@@ -86,30 +104,45 @@ def get_username():
             return username
 
 
-def clone_repo(repository):
-    """Clones the repository passed to it using the git shell interface."""
+def clone_repo(repository, arguments):
+    """Clones the repository passed to it using the git program."""
     devnull = open(os.devnull, "w")
     try:
         subprocess.check_call(["git", "clone", repository['git_url']],
                                 stderr=devnull)
     except subprocess.CalledProcessError:
-        while True:
-            choice = input("Repository {} already exists here, update? [Y/N] "
-                            .format(repository['name']))
-            if choice.lower() in ["y", "n"]:
-                break
-        if choice.lower() == 'y':
-            try:
-                os.chdir(repository['name'])
-            except:
-                print("Something's gone wrong, will ignore repository.")
-            else:
-                try:
-                    output = subprocess.check_output(["git", "pull"])
-                    print(output.decode(sys.stdout.encoding))
-                except subprocess.CalledProcessError:
-                    print("Something went badly wrong.")
-                os.chdir("..")
+        # Error thrown when repo exists locally.
+        if arguments['ud_all']:
+            update_repository(repository['name'])
+        elif should_update_repository(repository['name']):
+            update_repository(repository['name'])
+    else:
+        print("Cloned {} successfully.".format(repository['name']))
+
+
+def should_update_repository(repo_name):
+    """Provides a prompt to ask the user to update an existing repository."""
+    while True:
+        choice = input("Repository {} already exists here, update? [Y/N] "
+                        .format(repo_name))
+        if choice.lower() in ["y", "n"]:
+            break
+    return choice.lower() == 'y'
+
+
+def update_repository(repo_name):
+    """Updates a given repository."""
+    try:
+        os.chdir(repo_name)
+    except:
+        print("Something's gone wrong, will ignore repository.")
+    else:
+        try:
+            output = subprocess.check_output(["git", "pull"])
+            print(output.decode(sys.stdout.encoding))
+        except subprocess.CalledProcessError:
+            print("Something went badly wrong.")
+        os.chdir("..")
 
 
 def download_repos(repos, arguments):
@@ -117,15 +150,15 @@ def download_repos(repos, arguments):
     if repos:  # If list of repositories is not blank.
         for repo in repos:
             try:
-                if arguments['all']:
+                if arguments['dl_all']:
                     # Skip printing repo info for each one.
-                    clone_repo(repo)
+                    clone_repo(repo, arguments)
                 else:
                     print_repo_info(repo)  # Display name and description.
                     while True:
                         yesno = input("Clone repository? [Y/A/N/Q] ")
                         if yesno.lower() == 'y':
-                            clone_repo(repo)
+                            clone_repo(repo, arguments)
                             break
 
                         elif yesno.lower() == 'n':
@@ -133,8 +166,8 @@ def download_repos(repos, arguments):
                             break
 
                         elif yesno.lower() == 'a':
-                            clone_repo(repo)
-                            arguments['all'] = True
+                            clone_repo(repo, arguments)
+                            arguments['dl_all'] = True
                             break
 
                         elif yesno.lower() == 'q':
@@ -147,25 +180,37 @@ def download_repos(repos, arguments):
         print("User has no publicly available repositories.")
 
 
+def format_url(url):
+    """Ensures URL is using HTTPS protocol."""
+    return "https://" + url[6:]
+
+
 def main():
+    # Collect command-line switches.
     arguments = parse_args()
+
     # Get info if it hasn't been specified in parameters.
     if not arguments['username']:
         arguments['username'] = get_username()
-
 
     # Get info from fetched JSON data.
     my_api_url = api_url(arguments['username'])
     json_data = get_json(my_api_url)
 
-    if not arguments['all']:
-        arguments['all'] = get_all()
+    if not arguments['dl_all']:
+        arguments['dl_all'] = should_download_all()
+
+    if not arguments['ud_all']:
+        arguments['ud_all'] = should_update_all()
 
     # Make a shorter, more manageable list of dictionaries:
-    # (also makes sure github url is using HTTPS connection)
-    repos = [{'name': repo['name'],
-              'git_url': "https://" + repo['git_url'][6:],
-              'description': repo['description']} for repo in json_data]
+    repos = [
+            {'name': repo['name'],
+            'git_url': format_url(repo['git_url']),
+            'description': repo['description']}
+            for repo in json_data
+            ]
+
     download_repos(repos, arguments)
 
 
