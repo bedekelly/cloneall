e4e126f9fc49c05f893ae0a6a8e2ccbd3b2ad380
bedekelly <bedekelly97@gmail.com>
2014-06-18 00:15:06 +0100

    update for latest pypi release

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 from distutils.core import setup
 setup(name='cloneall',
-	  version='1.0.4',
+	  version='1.0.6',
 	  description="Clone GitHub repositories",
 	  author="Bede Kelly",
 	  author_email="bedekelly97@gmail.com",
 	  url="https://github.com/bedekelly/clone-them-all",
-	  requires=['SimpleMenu'],
-	  # dependency_links=['https://pypi.python.org/packages/source/S/SimpleMenu/SimpleMenu-1.0.tar.gz'],
-	  scripts=['cloneall'])
+	  scripts=['cloneall'],
+	  classifiers=[
+	  	"Intended Audience :: Developers",
+	  	"Environment :: Console :: Curses",
+	  	"Natural Language :: English",
+	  	"Operating System :: OS Independent",
+	  	"Programming Language :: Python :: 3",
+	  	"Programming Language :: Python :: Implementation :: CPython",
+	  	"Topic :: Software Development :: Version Control",
+	  	"Topic :: Utilities"
+	  	])
