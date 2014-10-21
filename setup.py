20f40e80380c8b1b88a4a47da12bcb2cab565268
Bede Kelly <bedekelly97@gmail.com>
2014-10-21 19:05:52 +0100

    fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from distutils.core import setup
 setup(name='cloneall',
-	  version='1.2.3',
+	  version='1.2.4',
 	  description="Clone GitHub repositories",
 	  author="Bede Kelly",
 	  author_email="bedekelly97@gmail.com",
